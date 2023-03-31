9eeae945b96f56cba12cc4b9852ddbb5ef1028f0
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-03-31 16:25:31 -0400

    trying stuff

--- app.py
+++ app.py
@@ -14,18 +14,25 @@ def home_post():
     convert_to=request.form['converting_to']
     from forex_python.converter import CurrencyCodes
     c = CurrencyCodes()
+    print(c)
     list_of_currencies=[]
-    for key in c._currency_data:
+    for key in c:
         list_of_currencies.append(key['cc'])
-    print(list_of_currencies)
+        print('list should be list' +list_of_currencies)
     if convert_from in list_of_currencies:
         amount=request.form['amount']
-        url = 'https://api.exchangerate.host/convert'
-        response = requests.get(url, params={'from':convert_from,'to':convert_to,'amount':amount})
-        data = response.json()
-        info=data['info']['rate']
-        total_converted=float(info) * float(amount)
-        return render_template('home.html',info=total_converted)
+        if amount != '':
+
+            url = 'https://api.exchangerate.host/convert'
+            response = requests.get(url, params={'from':convert_from,'to':convert_to,'amount':amount})
+            data = response.json()
+            info=data['info']['rate']
+            total_converted=float(info) * float(amount)
+            return render_template('home.html',info=total_converted)
+    else:
+        return '<h1>hi</h1>'
+
+
 
 
 
